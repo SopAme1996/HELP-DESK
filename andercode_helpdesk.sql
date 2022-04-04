9b9a48d82d804a0dcc5a9c68b6b43a047f2784bf
Luis Solis <le16081891@merida.tecnm.mx>
2022-04-04 15:45:27 -0500

    first commit

---
+++ andercode_helpdesk.sql
@@ -0,0 +1,194 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 26-09-2021 a las 19:00:27
+-- Versión del servidor: 10.1.38-MariaDB
+-- Versión de PHP: 5.6.40
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `andercode_helpdesk`
+--
+
+DELIMITER $$
+--
+-- Procedimientos
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_d_usuario_01` (IN `xusu_id` INT)  BEGIN
+	UPDATE tm_usuario 
+	SET 
+		est='0',
+		fech_elim = now() 
+	where usu_id=xusu_id;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_i_ticketdetalle_01` (IN `xtick_id` INT, IN `xusu_id` INT)  BEGIN
+	INSERT INTO td_ticketdetalle 
+    (tickd_id,tick_id,usu_id,tickd_descrip,fech_crea,est) 
+    VALUES 
+    (NULL,xtick_id,xusu_id,'Ticket Cerrado...',now(),'1');
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_l_usuario_01` ()  BEGIN
+	SELECT * FROM tm_usuario where est='1';
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_l_usuario_02` ()  BEGIN
+	SELECT * FROM tm_usuario where usu_id=xusu_id;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `td_ticketdetalle`
+--
+
+CREATE TABLE `td_ticketdetalle` (
+  `tickd_id` int(11) NOT NULL,
+  `tick_id` int(11) NOT NULL,
+  `usu_id` int(11) NOT NULL,
+  `tickd_descrip` mediumtext COLLATE utf8_spanish_ci NOT NULL,
+  `fech_crea` datetime NOT NULL,
+  `est` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tm_categoria`
+--
+
+CREATE TABLE `tm_categoria` (
+  `cat_id` int(11) NOT NULL,
+  `cat_nom` varchar(150) COLLATE utf8_spanish_ci NOT NULL,
+  `est` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `tm_categoria`
+--
+
+INSERT INTO `tm_categoria` (`cat_id`, `cat_nom`, `est`) VALUES
+(1, 'Harware', 1),
+(2, 'Software', 1),
+(3, 'Insidencia', 1),
+(4, 'Peticion de Servicio', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tm_ticket`
+--
+
+CREATE TABLE `tm_ticket` (
+  `tick_id` int(11) NOT NULL,
+  `usu_id` int(11) NOT NULL,
+  `cad_id` int(11) NOT NULL,
+  `tick_titulo` varchar(250) COLLATE utf8_spanish_ci NOT NULL,
+  `tick_descrip` mediumtext COLLATE utf8_spanish_ci NOT NULL,
+  `tick_estado` varchar(15) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `fech_crea` datetime DEFAULT NULL,
+  `est` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tm_usuario`
+--
+
+CREATE TABLE `tm_usuario` (
+  `usu_id` int(11) NOT NULL,
+  `usu_nom` varchar(150) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `usu_ape` varchar(150) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `usu_correo` varchar(150) COLLATE utf8_spanish_ci NOT NULL,
+  `usu_pass` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `rol_id` int(11) DEFAULT NULL,
+  `fech_crea` datetime DEFAULT NULL,
+  `fech_modi` datetime DEFAULT NULL,
+  `fech_elim` datetime DEFAULT NULL,
+  `est` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci COMMENT='Tabla Mantenedor de Usuarios';
+
+--
+-- Volcado de datos para la tabla `tm_usuario`
+--
+
+INSERT INTO `tm_usuario` (`usu_id`, `usu_nom`, `usu_ape`, `usu_correo`, `usu_pass`, `rol_id`, `fech_crea`, `fech_modi`, `fech_elim`, `est`) VALUES
+(5, 'prueba', 'prueba', 'prueba@prueba.com', '123456', 2, '2021-09-26 10:49:41', NULL, NULL, 1),
+(7, 'test', 'test', 'test@test.com', '123456', 1, '2021-09-26 10:55:57', NULL, NULL, 1);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `td_ticketdetalle`
+--
+ALTER TABLE `td_ticketdetalle`
+  ADD PRIMARY KEY (`tickd_id`);
+
+--
+-- Indices de la tabla `tm_categoria`
+--
+ALTER TABLE `tm_categoria`
+  ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Indices de la tabla `tm_ticket`
+--
+ALTER TABLE `tm_ticket`
+  ADD PRIMARY KEY (`tick_id`);
+
+--
+-- Indices de la tabla `tm_usuario`
+--
+ALTER TABLE `tm_usuario`
+  ADD PRIMARY KEY (`usu_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `td_ticketdetalle`
+--
+ALTER TABLE `td_ticketdetalle`
+  MODIFY `tickd_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tm_categoria`
+--
+ALTER TABLE `tm_categoria`
+  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `tm_ticket`
+--
+ALTER TABLE `tm_ticket`
+  MODIFY `tick_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tm_usuario`
+--
+ALTER TABLE `tm_usuario`
+  MODIFY `usu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
